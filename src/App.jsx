502ca06324a650c5d252b91f61e99be3d6b30808
varadisthedev <varadisthedev@gmail.com>
2025-12-06 01:24:10 +0530

    add: framer fade-in and integration with tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { motion } from "framer-motion";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <motion.div
+      initial={{ opacity: 0, x: -100 }} // before animation
+      animate={{ opacity: 1, x: 0 }} // after animation
+      transition={{ duration: 1 }}
+      className="w-[200px] h-[200px] bg-blue-300 border-2 border-blue-400 m-3"
+      // style={{
+      //   width: "200px",
+      //   height: "200px",
+      //   background: "lightblue",
+      //   borderRadius: "10px",
+      //   margin: "50px auto",
+      // }}
+    >
+      Hello world
+    </motion.div>
+  );
 }
 
-export default App
+export default App;
